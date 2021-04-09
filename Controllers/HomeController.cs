2038f2f6dc9082d3b53c900f66d50759e6b044e9
Jordan Khamvongsouk <jordankhamvongsouk@gmail.com>
2021-04-09 12:08:02 -0600

    Form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -60,10 +60,16 @@ namespace Intex_2.Controllers
             }) ;
         }
 
+        public IActionResult OsteologyForm()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
