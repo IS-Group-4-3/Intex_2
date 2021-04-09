b56a47cc708ebd0a347413e822aabb43b06f6d88
Daniel O'Hare <71663427+dohare-byu@users.noreply.github.com>
2021-04-09 11:31:11 -0600

    add location scaffolding

---
+++ Models/ViewModels/CombinedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex_2.Models.ViewModels
+{
+    public class CombinedViewModel
+    {
+        public GamousMain GamourMain { get; set; }
+        public GamousLocation GamousLocation { get; set; }
+    }
+}
