7a55e7002656aec342a97506aadae9c1101d73d6
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-04-11 02:57:40 -0600

    go tit to send the second action through

---
+++ Models/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex_2.Models.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public bool Selected { get; set; }
+
+    }
+}
